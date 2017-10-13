8090df5772987226b92101ccb360dfc8eada2913
KadrusBAG <32191410+KadrusBAG@users.noreply.github.com>
2017-10-13 12:24:49 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,12 +3,14 @@
 using namespace std;
 
 int main (){
-	string stroka;
-	int a[10], b[10]; int i; int p=0;
-	getline(cin, stroka);
-	istringstream stream(stroka);
-	for(i=0; i<10; ++i){
-	    if(!(stream>>a[i])){
+	string stroka1, stroka2;
+	int a[10], b[10], c[20]; int i; int p=0;
+	getline(cin, stroka1);
+	istringstream stream1(stroka1);
+	getline(cin, stroka2);
+	istringstream stream2(stroka2);
+	for(i=0; i<10; i++){
+	    if(!(stream1>>a[i])){
 	        cout<<"An error has occurred while reading the input sequence."<<endl;
 	        return -1;
 	    }
@@ -19,8 +21,8 @@ int main (){
 	    p=a[i];
 	}
 	p=0;
-	for(i=0; i<10; ++i){
-	    if(!(stream>>b[i])){
+	for(i=0; i<10; i++){
+	    if(!(stream2>>b[i])){
 	        cout<<"An error has occurred while reading the input sequence."<<endl;
 	        return -1;
 	    }
@@ -28,6 +30,28 @@ int main (){
 	        cout<<"The input sequence must be non-decreasing."<<endl;
 	        return -1;
 	    }
-	    p=a[i];
+	    p=b[i];
 	}
-	
+	for (int j = 0; j < 20; j++)
+	{
+		if (j < 10) c[j] = a[j];
+		else c[j] = b[j - 10];
+	}
+	for (int i = 0; i < 19 ; i++) 
+	{
+		for (int j = 0; j < 19-i ; j++)
+		{
+			if (c[j] > c[j + 1])
+			{
+				int time = c[j];
+				c[j] = c[j + 1];
+				c[j + 1] = time;
+			}
+		}
+	}
+	for (int j = 0; j < 20; j++)
+	{
+		cout << c[j]<< " ";
+	}
+	return 0;
+}
